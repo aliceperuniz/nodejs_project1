46a9d8fe42650ad619a097e78c6887e82094efc2
Alice Peruniz <apo2@cin.ufpe.br>
2023-10-29 14:15:05 -0300

    class 2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 //const http = require('http');
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
+    const { method, url } = req;
+
+    if(method === 'GET' && url === '/users') {
+        return res.end(JSON.stringify(users));
+    } 
+    if (method === 'POST' && url === '/users/1') {
+        users.push({
+            id: 1,
+            name: 'Alice Peruniz',
+            email: 'aliceperuniz@exemplo.com'
+        })
+        return res.end('Criação de usuário');
+    }
+
     return res.end('Hello World');
 });
 
